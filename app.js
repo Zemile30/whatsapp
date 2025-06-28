f5d6cb7ee7d55e867346233e01b38cd6be9ed3f8
Zemile30 <zemile.nesirli@gmail.com>
2025-06-28 17:56:14 +0000

    inline-block

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
 
 
 
-    $('#me').click(function (e) { 
+    $("#me").click(function (e) { 
         e.preventDefault()
         function myel(myclass = "") {
-            if(!$.trim($('#inputme').val()) == '') {
-                let vall = $('#inputme').val()
-                    , newH3 = $('<h3> </h3>')
-                    , newDiv = $('<div class="line"> </div>')
-                    , bigDiv = $(`<div ${myclass}> </div>`)
+            if(!$.trim($("#inputme").val()) == '') {
+                let vall = $("#inputme").val()
+                    let newH3 = $('<h3> </h3>')
+                    let newDiv = $('<div class="line"> </div>')
+                    let bigDiv = $(`<div ${myclass}> </div>`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
@@ -21,10 +21,11 @@ $(document).ready(function () {
             }
 
         }
-        $('#messageyou').append(myel())
-        $('#messageme').append(myel('class="end"'))
-        $('#inputme').val("")
-            
-    });
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)   
+    })
 
 });
