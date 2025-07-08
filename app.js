c8f96a6c6d95c9a1e5c0d98347743322e84cc903
Zemile30 <zemile.nesirli@gmail.com>
2025-07-08 16:46:57 +0000

    you

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ $(document).ready(function () {
         function myel(myclass = "") {
             if (!$.trim($("#inputme").val()) == '') {
                 let vall = $("#inputme").val()
-                    let newH3 = $('<h3> </h3>')
-                    let newDiv = $('<div class="line"> </div>')
-                    let bigDiv = $(`<div ${myclass}> </div>`)
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div ${myclass}> </div>`)
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
@@ -21,11 +21,31 @@ $(document).ready(function () {
             }
 
         }
-    $("#messageyou").append(myel())
-    $("#messageme").append(myel('class="end"'))
-    $("#inputme").val("")
-    document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-    document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)   
-})
-
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)   
+    })
+    $("#you").click(function (e) { 
+        e.preventDefault()
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == '') {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div ${myclass}> </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return
+            }
+        }
+        $("#messageyou").append(myel())
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+    })
 });
